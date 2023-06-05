b0e25f341a32071e2284d404f66f297509c6b630
Altycon <94148532+Altycon@users.noreply.github.com>
2023-06-05 03:59:07 -0700

    adding highlight of number functionality

--- js/index.js
+++ js/index.js
@@ -6,17 +6,52 @@ const DATA_DISPLAY = {
     TOTAL_DIGITS: document.querySelector('.total-digits'),
     TOTAL_NUMBERS: document.querySelector('.total-numbers'),
     CONTAINERS: {
-        NUMBER_DATA: document.querySelector('.number-data'),
+        NUMBER_DATA: document.querySelector('.numbers'),
         GROUP_DATA: document.querySelector('.group-data')
     }
 }
+function getNumberPlaces(num){
+    let places;
+    for(const prop in R_NUMBER_DATA.numbers){
+        if(prop === num){
+            places = [...R_NUMBER_DATA.numbers[prop].places]
+        }
+    }
+    return places;
+}
+function selectNumbersInGroup({currentTarget}){
+    const groups = [...document.querySelectorAll('.group')];
+    const currentNumber = currentTarget.dataset.number;
+    const numberIndexs = getNumberPlaces(currentNumber);
+    const numberLength = currentNumber.length;
 
+    // clear highlighted digits
+    groups.forEach( group => {
+        [...group.children].forEach( digit => {
+            if(digit.classList.contains('highlight')){
+                digit.classList.remove('highlight');
+            }
+        })
+    })
+
+    numberIndexs.forEach( position =>{
+        const row = position[0];
+        const col = position[1];
+        const group = groups[row].querySelectorAll('.digit');
+
+        for(let i = 0; i < numberLength; i++){
+            group[col + i].classList.add('highlight');
+        }
+    });
+    
+};
 function addDigitData(numbers){
     
     const df = new DocumentFragment();
     for(const num in numbers){
         //if(numbers[num].total > 300){
             const div = document.createElement('div');
+            div.dataset.number = num;
             const h3 = document.createElement('h3');
             h3.innerText = `${num}`
             const p1 = document.createElement('p');
@@ -29,16 +64,31 @@ function addDigitData(numbers){
             div.appendChild(p1);
             div.appendChild(p2);
             div.appendChild(p3);
+            div.addEventListener('click', selectNumbersInGroup);
             df.appendChild(div);
         //}
     }
     DATA_DISPLAY.CONTAINERS.NUMBER_DATA.appendChild(df);
 };
 function addGroupData(groups){
+    const parentWidth = +getComputedStyle(DATA_DISPLAY.CONTAINERS.GROUP_DATA).getPropertyValue('width').slice(0,-2);
+
     const df = new DocumentFragment();
-    groups.forEach( group => {
+    groups.forEach( (group,index) => {
         const p = document.createElement('p');
-        p.innerText = group;
+        p.setAttribute('class', 'group');
+        p.dataset.group = `${index}`;
+        const len = group.length;
+        const size = (parentWidth/len)*devicePixelRatio;
+        for(let i = 0; i < len; i++){
+            const span = document.createElement('span');
+            span.setAttribute('class', 'digit');
+            span.dataset.number = `${group[i]}`;
+            //span.setAttribute('width', size);
+            //span.style.border = `1px solid white`;
+            span.innerText = group[i];
+            p.appendChild(span);
+        }
         df.appendChild(p);
     });
     DATA_DISPLAY.CONTAINERS.GROUP_DATA.appendChild(df);
